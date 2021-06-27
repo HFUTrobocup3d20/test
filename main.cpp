92a553faecf534fabb81bae728f4014703ff2b39
IllyasvielEin <785291773@qq.com>
2021-06-27 23:52:45 +0800

    sth

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Illya on 2021/6/27.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "RoboCup3d_20." << endl;
+    /* My idea is that we everyone put sth representing someone identify
+    in main function, such as: */
+    cout << "Illyasviel." << endl;
+    /* try it */
+
+    return 0;
+}
+

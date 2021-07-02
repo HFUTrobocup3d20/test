b72750f1643d40348b733adf10552a8fad6b93e9
Illyasviel von Einzbern <69078819+IllyasvielEin@users.noreply.github.com>
2021-07-02 12:06:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,7 @@ using namespace std;
 int main()
 {
     cout << "RoboCup3d_20." << endl;
-    /* My idea is that we everyone put sth representing someone identify
-    in the main function, such as: */
     cout << "Illyasviel." << endl;
-    /* try it */
     //test1
 
     return 0;

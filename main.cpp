772487779f00a4720e10e39437091d72acce62ac
zhr <1224836506@qq.com>
2021-07-08 05:39:08 +0800

    Rewrite some of the notes.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ int main()
     cout << "RoboCup3d_20." << endl;
     cout << "Illyasviel & ResnowH." << endl;
     cout << "I am ResnowH, this is my first push." << endl;
+    /*this is interesting all right?*/
+    /*we can do this here and do not need to be anxious about our fault! */
     return 0;
 }
 
